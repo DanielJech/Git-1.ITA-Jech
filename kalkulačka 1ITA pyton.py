fd05fc1f5c2f39a75e51b387e3f55b6b70c87a5c
DanielJech <d.jech@student.iss-cheb.cz>
2026-02-04 09:07:05 +0100

    Add files via upload

---
+++ kalkulačka 1ITA pyton.py
@@ -0,0 +1,45 @@
+﻿print("Kalkulacka: + - * / sin cosinus tangus")
+operace = input()
+if operace == "+":
+    print("zadej první číslo")
+    a = float(input())
+    print("zadej druhý číslo")
+    b = float(input())
+    plus = a + b
+else:
+    if operace == "-":
+        print("zadej první číslo")
+        a = float(input())
+        print("zadej druhý číslo")
+        b = float(input())
+        minus = a - b
+    else:
+        if operace == "*":
+            print("zadej první číslo")
+            a = float(input())
+            print("zadej druhý číslo")
+            b = float(input())
+            krat = a * b
+        else:
+            if operace == "/":
+                print("zadej první číslo")
+                a = float(input())
+                print("zadej druhý číslo")
+                b = float(input())
+                deleno = a / b
+            else:
+                if operace == "sin":
+                    print("zadej první číslo")
+                    a = float(input())
+                    sinus = sin(a)
+                else:
+                    if operace == "cosinus":
+                        print("zadej první číslo")
+                        a = float(input())
+                        cosinus = cos(a)
+                    else:
+                        if operace == "tangus":
+                            print("zadej první číslo")
+                            a = float(input())
+                            tangus = tan(a)
+                        else:
